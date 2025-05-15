9b02f03f2271a210b9519adadbedba9a8e3d0d1e
Anthony <anthony@khisa.com>
2025-05-15 14:12:07 -0300

    added const wisdom

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function generateRandomNumber(num) {
     return Math.floor(Math.random()*num)
 };
-test
+const wisdom = {
+    yoursign: ["star", "moon", "sun", "comet", "black hole"],
+    fortune: ["good luck", "okay luck", "fantascti luck", "bad luck", "terrible luck"],
+    advise: ["trust no one", "eat all the sweets", "go out and enjoy nature", "bake a cake", "take this lightly"]
+}
