458000161f0b8bb226cfec28a0a8935015c83f66
Teresa <teresa@Teresas-MBP.hitronhub.home>
2025-05-15 12:58:23 -0300

    Added and reorganized some code

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
+
+//Created this function to select a random number from 0 - 6
 function generateRandomNumber(num) {
-    return Math.floor(Math.random()*num)
+    return Math.floor(Math.random()* num)
 };
 
+//Arrays to be iterated through, to create the message.
+
+
+
+
+// This is the array that will be used to display the personal message.
+let personalWisdom  = [yourSign, fortune, advice]
+
+//Arrays to be iterated through, to create the message.
+for (let )
